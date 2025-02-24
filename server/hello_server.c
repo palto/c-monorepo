8f0f8a265da3a7b39e4980615c7bda3a931b382d
Toni Paloniemi <toni.paloniemi@gmail.com>
2025-02-24 13:26:38 +0200

    Refactored hello_server to be more readable

--- server/hello_server.c
+++ server/hello_server.c
@@ -6,58 +6,24 @@
 #include <arpa/inet.h>
 #include <ctype.h>
 #include <signal.h>
-#include "hello_server.h"
 #include "trim.h"
 
 #define PORT 8080
 
 int server_socket;
 
-void handle_signal(int signal)
+// Function declarations
+static void handle_signal(int signal);
+static void handle_client(int client_socket);
+static void hello_server(void);
+
+int main()
 {
-    if (signal == SIGINT)
-    {
-        fprintf(stderr, "Shutting down server...\n");
-        close(server_socket);
-        exit(EXIT_SUCCESS);
-    }
+    hello_server();
+    return 0;
 }
 
-void handle_client(int client_socket)
-{
-    printf("Client connected\n");
-    char buffer[1024];
-    int bytes_read = recv(client_socket, buffer, 1024 - 1, 0);
-
-    if (bytes_read < 0)
-    {
-        perror("recv");
-    }
-    else
-    {
-        buffer[bytes_read] = '\0';
-        trim(buffer);
-        fprintf(stdout, "Received: %s\n", buffer);
-
-        // Open a stream for the client socket
-        FILE *client_stream = fdopen(client_socket, "w");
-        if (client_stream == NULL)
-        {
-            perror("fdopen");
-            close(client_socket);
-            return;
-        }
-
-        fprintf(client_stream, "Hello %s!\n", buffer);
-        fflush(client_stream); // Ensure the message is sent immediately
-
-        printf("Responding: Hello %.100s!\n", buffer);
-        fclose(client_stream); // This will also close the client socket
-    }
-    printf("Closing connection to client\n");
-}
-
-void hello_server()
+static void hello_server(void)
 {
     // Register signal handler
     signal(SIGINT, handle_signal);
@@ -111,7 +77,46 @@ void hello_server()
     }
 }
 
-int main()
+static void handle_client(int client_socket)
 {
-    hello_server();
+    printf("Client connected\n");
+    char buffer[1024];
+    int bytes_read = recv(client_socket, buffer, 1024 - 1, 0);
+
+    if (bytes_read < 0)
+    {
+        perror("recv");
+    }
+    else
+    {
+        buffer[bytes_read] = '\0';
+        trim(buffer);
+        fprintf(stdout, "Received: %s\n", buffer);
+
+        // Open a stream for the client socket
+        FILE *client_stream = fdopen(client_socket, "w");
+        if (client_stream == NULL)
+        {
+            perror("fdopen");
+            close(client_socket);
+            return;
+        }
+
+        fprintf(client_stream, "Hello %s!\n", buffer);
+        fflush(client_stream); // Ensure the message is sent immediately
+
+        printf("Responding: Hello %.100s!\n", buffer);
+        fclose(client_stream); // This will also close the client socket
+    }
+    printf("Closing connection to client\n");
+}
+
+static void handle_signal(int signal)
+{
+    if (signal == SIGINT)
+    {
+        fprintf(stderr, "Shutting down server...\n");
+        close(server_socket);
+        exit(EXIT_SUCCESS);
+    }
 }
--- server/hello_server.h
+++
@@ -1,6 +0,0 @@
-#ifndef HELLO_SERVER_H
-#define HELLO_SERVER_H
-
-void hello_server();
-
-#endif // HELLO_SERVER_H
